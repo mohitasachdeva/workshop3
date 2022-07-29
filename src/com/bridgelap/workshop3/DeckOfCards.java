c2c5b438ecdc1969736155569842e840f5f1f228
HP <sachdevamohita5@gmail.com>
2022-07-29 11:16:41 +0530

    uc1

---
+++ src/com/bridgelap/workshop3/DeckOfCards.java
@@ -0,0 +1,13 @@
+package com.bridgelap.workshop3;
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+
+
+        String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String player[][] = new String[4][9];
+        int n = SUITS.length * RANKS.length;
+         String [] deck = new String[n];
+    }
+}
